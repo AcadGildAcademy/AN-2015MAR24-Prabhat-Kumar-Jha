3612c17b48ed27ede447fe39087292a6a4a4ce35
pjha06 <prabhatjha25@gmail.com>
2015-05-18 21:10:05 +0530

    uploaded

---
+++ sqlitedemo/app/src/main/java/com/example/prabhat/sqlitedemo/MyObject.java
@@ -0,0 +1,14 @@
+package com.example.prabhat.sqlitedemo;
+
+/**
+ * Created by PRABHAT on 18-05-2015.
+ */
+public class MyObject {
+    public String objectName;
+
+    // constructor for adding sample data
+    public MyObject(String objectName){
+
+        this.objectName = objectName;
+    }
+}
